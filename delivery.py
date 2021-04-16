08a62d231bb6e2bbd9de60fdf743a469a79a995b
erik-fox <erikfox589@gmail.com>
2021-04-15 18:55:38 -0700

    Add files via upload

--- delivery.py
+++ delivery.py
@@ -1,9 +1,23 @@
 def delivery(who, distance):
 	fo=open("hw2.txt","r")	
 	N=int(fo.readline())
-	array=[]
+	best=[]
+	inquiry=[]
 	for i in range(N):
 		din=fo.readline()
+		din=din.rstrip('\n')
 		din=din.split(",")
-		array.append(din)
-	print(array)
+		if i == 0:
+			best.append(din)
+			if who==din[0]:
+				inquiry.append(din)
+		else:
+			if (((float(distance)/float(best[0][1]))*float(60))+float(best[0][2]))>(((float(distance)/float(din[1]))*float(60))+float(din[2])):
+				best.pop(0)
+				best.append(din)
+			if who== din[0]:
+				inquiry.append(din)
+	print('{} would get the package there in {} minutes' .format(inquiry[0][0],str(((float(distance)/float(inquiry[0][1]))*float(60))+float(inquiry[0][2]))))
+	if who!=best[0][0]:
+		print('BEST CHOICE: {} would get the package there in {} minutes' .format(best[0][0],str(((float(distance)/float(best[0][1]))*float(60))+float(best[0][2]))))
+
